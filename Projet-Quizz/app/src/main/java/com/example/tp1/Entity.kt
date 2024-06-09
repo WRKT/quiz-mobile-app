effd4dbda6e87cdd08528affbac88d5fe31b7a30
Winness R <winness.rakotozafy@outlook.fr>
2024-06-09 23:00:21 +0200

    feat: add foreign keys in entity table

--- Projet-Quizz/app/src/main/java/com/example/tp1/Entity.kt
+++ Projet-Quizz/app/src/main/java/com/example/tp1/Entity.kt
@@ -2,20 +2,37 @@ package com.example.tp1
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "questions")
+@Entity(
+    tableName = "questions",
+    foreignKeys = [ForeignKey(
+        entity = Category::class,
+        parentColumns = ["id"],
+        childColumns = ["categoryId"],
+        onDelete = ForeignKey.CASCADE
+    )]
+)
 data class Question(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
-    @ColumnInfo(name = "categoryId") val categoryId: Int,
+    @ColumnInfo(name = "categoryId", index = true) val categoryId: Int,
     @ColumnInfo(name = "text") val text: String,
     @ColumnInfo(name = "correctAnswer") val correctAnswer: String
 )
 
-@Entity(tableName = "answers")
+@Entity(
+    tableName = "answers",
+    foreignKeys = [ForeignKey(
+        entity = Question::class,
+        parentColumns = ["id"],
+        childColumns = ["questionId"],
+        onDelete = ForeignKey.CASCADE
+    )]
+)
 data class Answer(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
-    @ColumnInfo(name = "questionId") val questionId: Int,
+    @ColumnInfo(name = "questionId", index = true) val questionId: Int,
     @ColumnInfo(name = "text") val text: String
 )
 
